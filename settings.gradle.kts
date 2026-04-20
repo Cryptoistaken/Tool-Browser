f53a073e8a80b06f3d80bee35ca6e2c077a1d828
Cryptoistaken <traderspopy@gmail.com>
2026-04-20 11:50:04 +0600

    fix: move plugin versions into pluginManagement to resolve KSP on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,20 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.13.2"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+        id("com.google.devtools.ksp") version "2.0.21-1.0.29"
     }
 }
 
